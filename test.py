14838b66a61c29ddaaedb32e7f522989d44e187f
Hiroshan Madusanka <pmhiroshan@gmail.com>
2024-11-22 15:25:54 +0530

    CRUD DB

---
+++ test.py
@@ -0,0 +1,19 @@
+ if request.method == 'POST':
+        item_name = request.form['item_name']
+        quantity = int(request.form['quantity'])
+        price = float(request.form['price'])
+        reorder_level = int(request.form['reorder_level'])
+
+        # Insert new stock item into the database
+        cursor = mysql.connection.cursor()
+        cursor.execute('INSERT INTO stock (item_name, quantity, price, reorder_level) VALUES (%s, %s, %s, %s)',
+                       (item_name, quantity, price, reorder_level))
+        mysql.connection.commit()
+        cursor.close()
+        return redirect('/stock')
+
+    # Retrieve all stock items from the database
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM stock')
+    stock_items = cursor.fetchall()
+    cursor.close()
